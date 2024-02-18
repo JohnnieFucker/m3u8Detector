5289745bdfedf3cf573bd7afc3f8de856b7a78e2
shine <shine@vip.qq.com>
2024-02-18 08:31:19 +0800

    一键复制

--- background.js
+++ background.js
@@ -1,38 +1,38 @@
 let detectedRequests = [];
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    // 检查标签页是否正在加载新内容
-    if (changeInfo.status === 'loading') {
-        // 清空detectedRequests数组
-        detectedRequests = [];
+	// 检查标签页是否正在加载新内容
+	if (changeInfo.status === 'loading') {
+		// 清空detectedRequests数组
+		detectedRequests = [];
 
-        // 更新扩展图标上的角标，反映清空操作
-        chrome.action.setBadgeText({ text: '', tabId: tabId });
-    }
+		// 更新扩展图标上的角标，反映清空操作
+		chrome.action.setBadgeText({ text: '', tabId: tabId });
+	}
 });
 
 chrome.webRequest.onCompleted.addListener(
-    async (d) => {
-        const ext = (d.url.split('?')[0].split('.').pop() || '').toLowerCase();
-        if (ext === 'm3u8' || ext === 'm3u') {
-            console.log(d.url);
-            chrome.tabs.get(d.tabId, (tab) => {
-                detectedRequests.push({
-                    url: d.url,
-                    title: tab.title
-                });
-                // 更新插件图标上的计数
-                chrome.action.setBadgeText({ text: detectedRequests.length.toString(), tabId: d.tabId });
-            });
-        }
-    },
-    {
-        urls: ['<all_urls>']
-    }
+	async d => {
+		const ext = (d.url.split('?')[0].split('.').pop() || '').toLowerCase();
+		if (ext === 'm3u8' || ext === 'm3u') {
+			console.log(d.url);
+			chrome.tabs.get(d.tabId, tab => {
+				detectedRequests.push({
+					url: d.url,
+					title: tab.title
+				});
+				// 更新插件图标上的计数
+				chrome.action.setBadgeText({ text: detectedRequests.length.toString(), tabId: d.tabId });
+			});
+		}
+	},
+	{
+		urls: ['<all_urls>']
+	}
 );
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.msg === 'getUrls') {
-        sendResponse({ data: detectedRequests });
-    }
-    return true; // 异步响应
+	if (request.msg === 'getUrls') {
+		sendResponse({ data: detectedRequests });
+	}
+	return true; // 异步响应
 });
--- popup.js
+++ popup.js
@@ -1,10 +1,31 @@
 chrome.runtime.sendMessage({ msg: 'getUrls' }, response => {
-	console.log(response);
+	// console.log(response);
 	const list = document.getElementById('requests');
 	response.data.forEach(req => {
 		const item = document.createElement('li');
-		item.textContent = `opt.url = '${req.url}';\n
-                            opt.filmName = '${req.title}';`;
+		item.innerHTML = `<div class="divContent">
+                            <div>${req.url}|||${req.title}</div>
+                          </div>
+                          <div class="btn">copy</div>`;
 		list.appendChild(item);
+	});
+	const buttons = document.querySelectorAll('.btn');
+	buttons.forEach(button => {
+		button.addEventListener('click', event => {
+			console.log('copyItem');
+			// 使用 event.target 来获取触发事件的元素
+			const btn = event.target; // 获取实际点击的按钮
+			const ele = btn.closest('li').querySelector('.divContent'); // 找到按钮所在的列表项内的 divContent
+			const textToCopy = ele.textContent; // 获取要复制的文本
+			navigator.clipboard.writeText(textToCopy).then(
+				() => {
+					btn.textContent = 'done';
+					// alert('文本已复制');
+				},
+				err => {
+					// console.error('无法复制文本: ', err);
+				}
+			);
+		});
 	});
 });
